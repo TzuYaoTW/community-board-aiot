8e46b1d7098fd1f00453143d526eefcac1064969
TzuYao <chaotzuyao@gmail.com>
2022-07-24 16:09:06 +0800

    修改README.md、增加OpenAPI註解

--- src/main/java/com/tzuyao/communityboardaiot/controller/FacilityController.java
+++ src/main/java/com/tzuyao/communityboardaiot/controller/FacilityController.java
@@ -3,9 +3,15 @@ package com.tzuyao.communityboardaiot.controller;
 import com.tzuyao.communityboardaiot.constant.FacilityCategory;
 import com.tzuyao.communityboardaiot.dto.FacilityQueryParams;
 import com.tzuyao.communityboardaiot.dto.FacilityRequest;
+import com.tzuyao.communityboardaiot.model.Admin;
 import com.tzuyao.communityboardaiot.model.Facility;
 import com.tzuyao.communityboardaiot.service.FacilityService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +31,13 @@ public class FacilityController {
     @Autowired
     private FacilityService facilityService;
 
-    @Operation(summary = "取得設施預定列表")
+    @Operation(summary = "取得設施預定列表", description = "可模糊查詢姓名，" +
+            "可選擇場地，" +
+            "可選擇日期，" +
+            "可選擇時段，" +
+            "可以排除掉已過期資料(也可以全部都顯示)，" +
+            "依照前端請求orderBy指定項目排序(預設為預定日期時間)，" +
+            "依照前端請求sort指定排序順序(預設ASC由小到大排序)，")
     @GetMapping("/facilities")
     public ResponseEntity<List<Facility>> getFacilities(@RequestParam(required = false) String name,
                                                         @RequestParam(required = false) FacilityCategory category,
@@ -66,6 +78,18 @@ public class FacilityController {
     }
 
     @Operation(summary = "修改預約", description = "透過facility_id取得設施預定資料後修改")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Admin.class))
+                            )
+                    }),
+            @ApiResponse(responseCode = "404", description = "無此 設施 ID，無法執行修改功能", content = {
+                    @Content()
+            })
+    })
     @PutMapping("/facilities/{facilityId}")
     public ResponseEntity<Facility> updateFacility(@PathVariable Integer facilityId,
                                                    @RequestBody FacilityRequest facilityRequest){
--- src/main/java/com/tzuyao/communityboardaiot/model/Facility.java
+++ src/main/java/com/tzuyao/communityboardaiot/model/Facility.java
@@ -1,6 +1,7 @@
 package com.tzuyao.communityboardaiot.model;
 
 import com.tzuyao.communityboardaiot.constant.FacilityCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,12 +9,27 @@ import java.util.Date;
 
 @Data
 public class Facility {
+    @Schema(description = "公共設施 ID")
     private Integer facilityId;
+
+    @Schema(description = "租借人地址")
     private String userAddress;
+
+    @Schema(description = "租借人姓名")
     private String userName;
+
+    @Schema(description = "租借人租的設施")
     private FacilityCategory facilityCategory;
+
+    @Schema(description = "預定的日期")
     private LocalDate reservedDay;
+
+    @Schema(description = "預定的時間(ex:10:00-12:00)")
     private String reservedTime;
+
+    @Schema(description = "創建預定數據時間")
     private Date createdDate;
+
+    @Schema(description = "最後修改預定數據時間")
     private Date lastModifiedDate;
 }
